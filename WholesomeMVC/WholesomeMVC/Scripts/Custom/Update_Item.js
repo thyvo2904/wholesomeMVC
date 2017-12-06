c5a21e326e3c196ad59a2d7ad3e87c9b133cd211
Charles Moore <moore3cd@dukes.jmu.edu>
2017-12-06 07:23:27 -0500

    merge

--- WholesomeMVC/WholesomeMVC/Scripts/Custom/Update_Item.js
+++ WholesomeMVC/WholesomeMVC/Scripts/Custom/Update_Item.js
@@ -1,35 +1,15 @@
-﻿$(function () {
-    $('[id*=gridMatchedCeresIDS]').footable();
+﻿$(document).ready(function () {
+	$('.equal-height').matchHeight({
+		byRow: true,
+		property: 'height',
+		target: null,
+		remove: false
+	});
 });
 
-function showDiv(elem) {
-    if (elem.value === 0) {
-        document.getElementById('#divold').style.display = "block";
-        document.getElementById('#divnew').style.display = "none";
-        document.getElementById('#divgridview').style.display = "none";
-    } else if (elem.value === 1) {
-        document.getElementById('#divnew').style.display = "block";
-        document.getElementById('#divold').style.display = "none";
-        document.getElementById('#divnew').style.display = "none";
-    } else if (elem.value === 2) {
-        document.getElementById('#divnew').style.display = "none";
-        document.getElementById('#divold').style.display = "none";
-        document.getElementById('#divgridview').style.display = "block";
-
-    } else {
-        document.getElementById('#divold').style.display = "block";
-        document.getElementById('#divnew').style.display = "none";
-        document.getElementById('#divgridview').style.display = "block";
-    }
-}
-
-$(document).ready(function () {
-
-    // Custom settings
-    $('.matchedCeresIDS').responsiveTable({
-        staticColumns: 2,
-        scrollRight: true,
-        scrollHintEnabled: true,
-        scrollHintDuration: 2000
-    });
+$(".expend-button").click(function () {
+	$("#hidden_ceres_name").val($(this).siblings(".hidden_ceres_name").val());
+	$("#hidden_ceresid").val($(this).siblings(".hidden_ceresid").val());
+	$("#hidden_ndbno").val($(this).siblings(".hidden_ndbno").val());
+	$("#button_expand_item").click();
 });
