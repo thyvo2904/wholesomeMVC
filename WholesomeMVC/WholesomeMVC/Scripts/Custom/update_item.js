db3935679d6780b240bbadb40a29a06927c82e57
Charles Moore <moore3cd@dukes.jmu.edu>
2017-12-06 09:31:04 -0500

    same

--- WholesomeMVC/WholesomeMVC/Scripts/Custom/update_item.js
+++ WholesomeMVC/WholesomeMVC/Scripts/Custom/update_item.js
@@ -13,3 +13,28 @@ $(".expend-button").click(function () {
 	$("#hidden_ndbno").val($(this).siblings(".hidden_ndbno").val());
 	$("#button_expand_item").click();
 });
+
+$(function () {
+    $('[id*=gridMatchedCeresIDS]').footable();
+});
+
+function showDiv(elem) {
+    if (elem.value === 0) {
+        document.getElementById('document.getElementById("divold")').style.display = "block";
+        document.getElementById('divnew').style.display = "none";
+        document.getElementById('divgridview').style.display = "none";
+    } else if (elem.value === 1) {
+        document.getElementById('divnew').style.display = "block";
+        document.getElementById('divold').style.display = "none";
+        document.getElementById('divnew').style.display = "none";
+    } else if (elem.value === 2) {
+        document.getElementById('divnew').style.display = "none";
+        document.getElementById('divold').style.display = "none";
+        document.getElementById('divgridview').style.display = "block";
+
+    } else {
+        document.getElementById('divold').style.display = "block";
+        document.getElementById('divnew').style.display = "none";
+        document.getElementById('divgridview').style.display = "block";
+    }
+}
