6d591796dd57d11f245fd6cdfc3cf293d1f9ba8e
Charles Moore <moore3cd@dukes.jmu.edu>
2017-12-05 23:24:49 -0500

    update item connected to master page

--- WholesomeMVC/WholesomeMVC/WebForms/Update_Item.aspx.designer.cs
+++ WholesomeMVC/WholesomeMVC/WebForms/Update_Item.aspx.designer.cs
@@ -13,42 +13,6 @@ namespace WholesomeMVC.WebForms {
     public partial class Update_Item {
         
         /// <summary>
-        /// ddlCategory control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.DropDownList ddlCategory;
-        
-        /// <summary>
-        /// Category control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.SqlDataSource Category;
-        
-        /// <summary>
-        /// txtSearch control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.TextBox txtSearch;
-        
-        /// <summary>
-        /// lblName control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Label lblName;
-        
-        /// <summary>
         /// gridMatchedCeresIDS control.
         /// </summary>
         /// <remarks>
