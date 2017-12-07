a1c9fb44a8d82cce59fcf6e7e74a08e00b0f668c
Yihui Zhou <zhou2yx@dukes.jmu.edu>
2017-12-06 21:53:26 -0500

    format ndscore table cell in comparision tale copy past all the update changes from linh

--- WholesomeMVC/WholesomeMVC/Scripts/Custom/Update_Item.js
+++ WholesomeMVC/WholesomeMVC/Scripts/Custom/Update_Item.js
@@ -1,33 +1,33 @@
 $(document).ready(function () {
-	$("#gridMatchedCeresIDS").footable({
-		// options go here
-		"expandFirst": false,
-		"showToggle": true,
-		"paging": {
-			"enabled": true,
-			"position": "left"
-		},
-		"sorting": {
-			"enabled": true
-		},
-		"filtering": {
-			"enabled": true,
-			"delay": 0,
-			"dropdownTitle": "Search in:",
-			"position": "right",
-		},
-		"editing": {
-			"enabled": true,
-			"allowDelete": false,
-			"editRow": function (row) {
-				$("#hidden_ceresid").val(row.value["col1"]);
-				$("#hidden_ceres_name").val(row.value["col2"]);
-				$("#hidden_ndbno").val(row.value["col5"]);
-				$(".modal").modal("show");
-				$("#button_expand_item").click();
-			}
-		}
-	});
+    $("#gridMatchedCeresIDS").footable({
+        // options go here
+        "expandFirst": false,
+        "showToggle": true,
+        "paging": {
+            "enabled": true,
+            "position": "left"
+        },
+        "sorting": {
+            "enabled": true
+        },
+        "filtering": {
+            "enabled": true,
+            "delay": 0,
+            "dropdownTitle": "Search in:",
+            "position": "right",
+        },
+        "editing": {
+            "enabled": true,
+            "allowDelete": false,
+            "editRow": function (row) {
+                $("#hidden_ceresid").val(row.value["col1"]);
+                $("#hidden_ceres_name").val(row.value["col2"]);
+                $("#hidden_ndbno").val(row.value["col5"]);
+                $(".modal").modal("show");
+                $("#button_expand_item").click();
+            }
+        }
+    });
 });
 
 function showDiv(elem) {
