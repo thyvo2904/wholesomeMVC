7d4c094b4d14d7a9c35a426b6306d0fa54fac679
Linh Nguyen <linhvng.2212@gmail.com>
2017-12-08 09:24:41 -0500

    revert to log off from manage account;

--- WholesomeMVC/WholesomeMVC/WebForms/_layout.Master.cs
+++ WholesomeMVC/WholesomeMVC/WebForms/_layout.Master.cs
@@ -12,111 +12,14 @@ namespace WholesomeMVC.WebForms
 {
 	public partial class _layout : System.Web.UI.MasterPage
     {
-		//private const string AntiXsrfTokenKey = "__AntiXsrfToken";
-		//private const string AntiXsrfUserNameKey = "__AntiXsrfUserName";
-		//private string _antiXsrfTokenValue;
-
-		//protected void Page_Init(object sender, EventArgs e)
-		//{
-		//	//First, check for the existence of the Anti-XSS cookie
-		//	var requestCookie = Request.Cookies[AntiXsrfTokenKey];
-		//	Guid requestCookieGuidValue;
-
-		//	//If the CSRF cookie is found, parse the token from the cookie.
-		//	//Then, set the global page variable and view state user
-		//	//key. The global variable will be used to validate that it matches 
-		//	//in the view state form field in the Page.PreLoad method.
-		//	if (requestCookie != null
-		//		&& Guid.TryParse(requestCookie.Value, out requestCookieGuidValue))
-		//	{
-		//		//Set the global token variable so the cookie value can be
-		//		//validated against the value in the view state form field in
-		//		//the Page.PreLoad method.
-		//		_antiXsrfTokenValue = requestCookie.Value;
-
-		//		//Set the view state user key, which will be validated by the
-		//		//framework during each request
-		//		Page.ViewStateUserKey = _antiXsrfTokenValue;
-		//	}
-		//	//If the CSRF cookie is not found, then this is a new session.
-		//	else
-		//	{
-		//		//Generate a new Anti-XSRF token
-		//		_antiXsrfTokenValue = Guid.NewGuid().ToString("N");
-
-		//		//Set the view state user key, which will be validated by the
-		//		//framework during each request
-		//		Page.ViewStateUserKey = _antiXsrfTokenValue;
-
-		//		//Create the non-persistent CSRF cookie
-		//		var responseCookie = new HttpCookie(AntiXsrfTokenKey) {
-		//			//Set the HttpOnly property to prevent the cookie from
-		//			//being accessed by client side script
-		//			HttpOnly = true,
-
-		//			//Add the Anti-XSRF token to the cookie value
-		//			Value = _antiXsrfTokenValue
-		//		};
-
-		//		//If we are using SSL, the cookie should be set to secure to
-		//		//prevent it from being sent over HTTP connections
-		//		if (FormsAuthentication.RequireSSL &&
-		//			Request.IsSecureConnection)
-		//		{
-		//			responseCookie.Secure = true;
-		//		}
-
-		//		//Add the CSRF cookie to the response
-		//		Response.Cookies.Set(responseCookie);
-		//	}
-
-		//	Page.PreLoad += master_Page_PreLoad;
-		//}
-
-		//protected void master_Page_PreLoad(object sender, EventArgs e)
-		//{
-		//	//During the initial page load, add the Anti-XSRF token and user
-		//	//name to the ViewState
-		//	if (!IsPostBack)
-		//	{
-		//		//Set Anti-XSRF token
-		//		ViewState[AntiXsrfTokenKey] = Page.ViewStateUserKey;
-
-		//		//If a user name is assigned, set the user name
-		//		ViewState[AntiXsrfUserNameKey] =
-		//			   Context.User.Identity.Name ?? String.Empty;
-		//	}
-		//	//During all subsequent post backs to the page, the token value from
-		//	//the cookie should be validated against the token in the view state
-		//	//form field. Additionally user name should be compared to the
-		//	//authenticated users name
-		//	else
-		//	{
-		//		//Validate the Anti-XSRF token
-		//		if ((string) ViewState[AntiXsrfTokenKey] != _antiXsrfTokenValue
-		//			|| (string) ViewState[AntiXsrfUserNameKey] !=
-		//				 (Context.User.Identity.Name ?? String.Empty))
-		//		{
-		//			throw new InvalidOperationException("Validation of " +
-		//								"Anti-XSRF token failed.");
-		//		}
-		//	}
-		//}
-
 		protected void Page_Load(object sender, EventArgs e)
         {
             label_year.Text = DateTime.Now.Year.ToString();
-            if(HttpContext.Current.User.IsInRole("Admin"))
-            {
-                log_out.Visible = false;
-            }
              
             if (Request.IsAuthenticated) {
 				// User is authenticated
-				//log_in_out.NavigateUrl = "~/Manage/Index";
-				//log_in_out.NavigateUrl = "javascript:document.getElementById('logoutForm').submit()";
-				log_in.Visible = false;
-                log_out.Visible = true;
+				authentication.NavigateUrl = "~/Manage/Index";
+				authentication.Text = "Account";
 				label_user.Text = HttpContext.Current.User.Identity.GetUserName();
 
                 String ConnectionString = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
@@ -150,19 +53,17 @@ namespace WholesomeMVC.WebForms
                         command1.ExecuteNonQuery();
                         connection.Close();
                     }
-
-                    
                 }
                 catch (Exception q)
                 {
                     Console.WriteLine(q.ToString());
                 }
-
             } else {
 				// User is NOT authenticated
+				authentication.NavigateUrl = "~/Account/Login";
+				authentication.Text = "Login";
 				label_user.Text = "Account";
 			}
-
 		}
 
 		protected void btnSearch(object sender, EventArgs e)
@@ -209,33 +110,5 @@ namespace WholesomeMVC.WebForms
 			WebForms.FoodItem.findNdbno(foodSearch);
 			Server.Transfer("/WebForms/indexresult.aspx");
 		}
-
-		/***
-		 * Generate HTML code to create a form and submit to ~/Account/Logoff to logout.
-		 * Utilizing ~/Account/Logoff will prevent CSRF attacks.
-		 */
-		protected void LogOut(object sender, EventArgs e)
-		{
-			StringBuilder sbRenderOnMe = new StringBuilder();
-
-			// the form and the data
-			//sbRenderOnMe.AppendFormat(@"
-			//<html>
-			//	<body>
-			//		<form action='/Account/LogOff' class='hidden' id='logoutForm' method='post'>
-			//			<input id='tokenToSubmit' name='__RequestVerificationToken' type ='hidden' value='{0}' />
-			//		</form>",
-			//AntiForgery.GetHtml());
-
-			// the auto submit
-			sbRenderOnMe.AppendFormat("<script>javascript:document.getElementById('logoutForm').submit();</script>");
-			sbRenderOnMe.AppendFormat("</body></html>");
-            HttpContext.Current.Session.Clear();
-
-
-			//Response.Write(sbRenderOnMe.ToString());
-			//Response.End();
-            
-        }
 	}
 }
